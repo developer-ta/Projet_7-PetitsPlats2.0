3216a3021891dcc0d8b315247b9d63e000e65729
developer-ta <78349294+developer-ta@users.noreply.github.com>
2024-04-26 12:27:19 +0200

    fin projet solution A

--- src/models/RecipeModel.js
+++ src/models/RecipeModel.js
@@ -78,15 +78,7 @@ export class RecipeModel {
 	*/
 	prototypeSearchModelBuilder(recipes) {
 
-
-
-
-
 		if (isExciteOrNotEmpty(recipes)) {
-
-			//tri by alphabet
-			//recipes.sort((a, b) => (a.name).localeCompare(b.name));
-
 
 			//build search obj list
 			const searchList = recipes.map(({ name, ingredients, description }, index) => {
@@ -123,42 +115,3 @@ export class RecipeModel {
 
 
 }
-//Le système recherche des recettes correspondant à l’entrée utilisateur dans : le
-// titre de la recette, la liste des ingrédients de la recette, la description de la
-// recette.
-//name,ingrédients,description
-
-// {
-// 	"id": 1,
-// 	"image": "Recette01.jpg",
-// 	"name": "Limonade de Coco",
-// 	"servings": 1,
-// 	"ingredients": [
-// 	  {
-// 	    "ingredient": "Lait de coco",
-// 	    "quantity": 400,
-// 	    "unit": "ml"
-// 	  },
-// 	  {
-// 	    "ingredient": "Jus de citron",
-// 	    "quantity": 2
-// 	  },
-// 	  {
-// 	    "ingredient": "Crème de coco",
-// 	    "quantity": 2,
-// 	    "unit": "cuillères à soupe"
-// 	  },
-// 	  {
-// 	    "ingredient": "Sucre",
-// 	    "quantity": 30,
-// 	    "unit": "grammes"
-// 	  },
-// 	  {
-// 	    "ingredient": "Glaçons"
-// 	  }
-// 	],
-// 	"time": 10,
-// 	"description": "Mettre les glaçons à votre goût dans le blender, ajouter le lait, la crème de coco, le jus de 2 citrons et le sucre. Mixer jusqu'à avoir la consistence désirée",
-// 	"appliance": "Blender",
-// 	"ustensils": ["cuillère à Soupe", "verres", "presse citron"]
-//    },
